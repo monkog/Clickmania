4329b58d44929cf5d6b9083200255cf8bf3e8db6
monkog <monkog@wp.pl>
2019-05-26 22:04:00 +0200

    Add documentation tags to Board object

--- Clickmania/Board.cs
+++ Clickmania/Board.cs
@@ -9,10 +9,19 @@ namespace Clickmania
 
 		private readonly Random _random = new Random();
 
+		/// <summary>
+		/// Gets the number of columns.
+		/// </summary>
 		public int Width { get; }
 
+		/// <summary>
+		/// Gets the number of rows.
+		/// </summary>
 		public int Height { get; }
 
+		/// <summary>
+		/// Gets the number of different colors used in the game.
+		/// </summary>
 		public int ColorNumber { get; }
 
 		public Board(int width, int height, int colorNumber)
@@ -22,6 +31,10 @@ namespace Clickmania
 			ColorNumber = colorNumber;
 		}
 
+		/// <summary>
+		/// Gets the next generated color.
+		/// </summary>
+		/// <returns>Generated color.</returns>
 		public Color GetColor()
 		{
 			return _colorTab[_random.Next() % ColorNumber];
